2eee90a9c14993058ede31a65583c68afc10f485
Hiago-lc <111469804+Hiago-lc@users.noreply.github.com>
2022-08-21 12:43:13 -0300

    Jogo da Memoria - Hiago Lucas Santos Oliveira

---
+++ Jogo/js/game.js
@@ -0,0 +1,160 @@
+
+const grid = document.querySelector('.grid');
+
+const timer = document.querySelector('.timer');
+
+const spanPlayer = document.querySelector('.player');
+
+const caracter = [
+    '1' ,
+    '2' ,
+    '3' ,
+    '4' ,
+    '5' ,
+    '6' ,
+    '7' ,
+    '8' ,
+    '9' ,
+    '10' ,
+];
+    
+
+
+const createElement = (tag,  className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+
+}
+
+let firstCard = '';
+let secondCard = '';
+
+
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    if (disabledCards.length == 20) {
+        clearInterval(this.loop);
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
+    }
+}
+
+const checkCards = () => {
+    const firstCaracter = firstCard.getAttribute('data-caracter');
+    const secondCaracter = secondCard.getAttribute('data-caracter');
+
+    if (firstCaracter == secondCaracter) {
+
+
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
+
+        firstCard = '';
+        secondCard = '';
+
+        checkEndGame();
+
+
+
+    } else {
+
+        setTimeout(() => {
+
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+
+        }, 500)
+
+       
+    }
+
+}
+
+const revealCard = ({ target }) => {
+
+    if (target.parentNode.className.includes('reveal-card')) {
+        return;
+    }
+
+
+    if (firstCard == '') {
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+
+    } else if (secondCard == '') {
+
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+
+        checkCards();
+    }
+
+    
+}
+ 
+
+const createCard = (caracter) => {
+
+
+    const card = createElement('div' , 'card')
+    const front = createElement('div' , 'face front')
+    const back = createElement('div' , 'face back')
+
+    front.style.backgroundImage = `url('../Cs/${caracter}.jpg')`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener('click', revealCard);
+    card.setAttribute('data-caracter', caracter)
+
+    return card;
+
+
+
+
+}
+
+
+const loadGame = () => {
+    
+    const duplicateCaracter = [ ...caracter, ...caracter ];
+
+
+    const shuffledArray = duplicateCaracter.sort(() => Math.random() - 0.5);
+
+
+    duplicateCaracter.forEach((caracter) => {
+
+        const card = createCard(caracter);
+        grid.appendChild(card);
+    
+    });
+}
+
+const startTimer = () => {
+
+
+    this.loop = setInterval(() => {
+        const currentTime = Number(timer.innerHTML) ;
+        timer.innerHTML = currentTime + 1;
+
+
+    }, 1000);
+
+}
+
+window.onload = () => {
+
+    const playerName = localStorage.getItem('player');
+    spanPlayer.innerHTML = playerName;
+    startTimer();
+    loadGame();
+}
+
+
